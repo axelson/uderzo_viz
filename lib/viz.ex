89dda9fe337b084fe9a56b6f60878ca069d57a4e
Jason Axelson <jason.axelson@gmail.com>
2018-06-21 08:03:56 -1000

    Initial commit

---
+++ lib/viz.ex
@@ -0,0 +1,18 @@
+defmodule Viz do
+  @moduledoc """
+  Documentation for Viz.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Viz.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
