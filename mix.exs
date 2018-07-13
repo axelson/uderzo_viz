78dd7bdb184730942322c5e4939cddf987cc4568
Jason Axelson <jason.axelson@gmail.com>
2018-07-13 08:09:02 -1000

    Add special compilation bits to mix file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Viz.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      make_env: &make_env/0,
+      compilers: Mix.compilers ++ [:clixir, :elixir_make]
     ]
   end
 
@@ -16,6 +18,23 @@ defmodule Viz.Mixfile do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp make_env do
+    erl_env = case System.get_env("ERL_EI_INCLUDE_DIR") do
+                nil ->
+                  %{
+                    "ERL_EI_INCLUDE_DIR" => "#{:code.root_dir()}/usr/include",
+                    "ERL_EI_LIBDIR" => "#{:code.root_dir()}/usr/lib"
+                  }
+                _ ->
+                  %{}
+              end
+
+    erl_env
+    |> Map.put("MIX_ENV", "#{Mix.env}")
+    |> Map.put("CLIXIR_DIR", Mix.Project.build_path <> "/lib/clixir/priv")
+    |> Map.put("UDERZO_DIR", Mix.Project.build_path <> "/lib/uderzo/priv")
   end
 
   # Run "mix help deps" to learn about dependencies.
